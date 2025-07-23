bdb60110d0feecf7b0ee5f314461b61553a1239b
sanjay <sanjusanjay0444@gmail.com>
2025-07-23 23:47:10 +0530

    add redux pesist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,27 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 // filepath: /Users/sanjays/Desktop/mern-auth/client/src/redux/store.js
 import userReducer from '../user/userSlice'; // Adjust path if needed
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import persistStore from 'redux-persist/es/persistStore';
 
+
+const rootReducer = combineReducers({ user: userReducer})
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {user: userReducer},
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: false,
         }),
 });
+
+export const persistor = persistStore(store);
