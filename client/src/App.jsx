801efba1ca7ffcd0d8c40f55bb939fc4fa5ec0a1
sanjay <sanjusanjay0444@gmail.com>
2025-07-23 17:00:54 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,13 @@ import About from './pages/About';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 const App = () => {
   return (
   <BrowserRouter>
+  {/* header */}
+  <Header/>
+
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/about" element={<About />} />
