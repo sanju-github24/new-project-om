73468a8ba39212dea44d2bd34eb14f8ca3934273
sanjay <sanjusanjay0444@gmail.com>
2025-07-23 21:14:58 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
@@ -33,3 +35,23 @@ export const signup = async (req, res, next) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+
+export const signin = async (req, res, next) => {
+  const {email,password} = req.body;
+  try{
+    const validUser = await User.findOne({email});
+    if(!validUser) return next(errorHandler(404, "User not found"));
+    const ValidPassword = bcryptjs.compareSync(password, validUser.password);
+    if(!ValidPassword) return next(errorHandler(401, "wrong credentials"));
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+    const {password: hashedPassword, ...rest} = validUser._doc;
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60, // 1 hour in milliseconds
+      expires: new Date(Date.now() + 1000 * 60 * 60) // 1 hour from now
+    }).status(200).json(rest);
+  }catch (error){
+    next(error);
+  }
+}
